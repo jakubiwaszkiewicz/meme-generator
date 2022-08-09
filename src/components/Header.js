27ae976ef86facb5133c603099d40651fa2d6f5b
tomaszelby2 <102925580+jakubiwaszkiewicz@users.noreply.github.com>
2022-08-09 15:27:25 +0200

    making a Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
+import gigachad from "../images/giga-chad.png"
+
 function Header () {
     return (
         <header>
-
+            <img src={gigachad} alt=""/>
+            <h1>Meme Generator</h1>
+            <h4>Make some meme</h4>
         </header>
     )}
 export default Header;
